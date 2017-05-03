93483bd9117e572ff3eaf44449138f7078dcfbda
Hiroshi Uyama <bonjamin.jan26@gmail.com>
2017-05-03 22:27:59 +0900

    とりあえずBootstrapを導入

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,3 +1,10 @@
     <em>&copy; 2015 UYAMADOTCOFFEE</em>
+
+    <!-- http://getbootstrap.com/getting-started/ -->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) v1.12.4 -->
+    <script src="<?= base_url(); ?>assets/js/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="<?= base_url(); ?>assets/js/bootstrap.min.js"></script>
   </body>
 </html>
